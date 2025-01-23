3085f44751d56ecea7915e7f45607adbed86d1aa
Kate Keiroz <81806904+katekeiroz-dev@users.noreply.github.com>
2025-01-23 11:00:41 +0000

    DTO updated

--- src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -6,17 +6,10 @@ public class GameListDTO {
 
 	private Long id;
 	private String name;
-	
+
 	public GameListDTO(GameList entity) {
 		id = entity.getId();
 		name = entity.getName();
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
 }
+
